727b2c77fdc045466a39d76a9b230a44f6fbc40b
trulyno <1petrovdaniel1@gmail.com>
2025-10-28 21:29:14 +0200

    centrul 7

--- src/sites/7_a_balti/acte-nationale.php
+++ src/sites/7_a_balti/acte-nationale.php
@@ -64,59 +64,25 @@ function document($src, $title = '') {
                     <div class="documents-grid">
 
                         <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=112685&lang=ro", "Codul Familiei al Republicii Moldova");
-                        ?>
-
-                        
-                        <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=132934&lang=ro", "Legea nr. 547 din 25.12.2003 Asistența Socială");
-                        ?>
-
-                         <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=146836&lang=ro#", "Legea nr. 140 din 14.06.2011 Privind protecția specială a copiilor aflați în situație de risc și a copiilor separați de părinți");
-                        ?>
-
-                        <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=141516&lang=ro#", "Legea nr. 123 din 18.06.2010 Cu privire la serviciile sociale");
-                        ?>
-
-                        <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=138813&lang=ro#", "Legea nr. 99 din 28.05.2010 Privind regimul juridic al adopției");
-                        ?>
-
-                         <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=140852&lang=ro#", "Legea nr. 338 din 15.12.1994 Privind drepturile copilului");
-                        ?>
-
-                         <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=146155&lang=ro#", "Legea nr. 60 din 30.03.2012 Privind incluziunea socială a persoanelor cu dizabilități");
-                        ?>
-
-                        <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=103705&lang=ro#", "Hotărârea de Guvern nr. 450 din 28.04.2006 Pentru aprobarea Standartelor minime de calitate privind îngrijirea, educarea și socializarea copilului din Centrul de plasament temporar");
-                        ?>
-
-                         <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=101381&lang=ro", "Hotărârea Guvernului nr. 591 din 24 iulie 2017: Aprobă Regulamentul-cadru privind organizarea și funcționarea Serviciilor sociale de tip centru de plasament și standardele minime de calitate.");
-                        ?>
-
-                         <?php
-                           document("https://social.gov.md/wp-content/uploads/2024/05/Ordin-nr.-114_24.05.2024_Politica-interna-de-protectie.pdf", "Ordinul nr. 114 din 24 mai 2024: Aprobă Ghidul privind Politica internă de protecție a copilului.");
-                        ?>
-
-                        <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=113032&lang=ro", "Codul Muncii al Republicii Moldova: Aplicabil tuturor angajatorilor, inclusiv instituțiilor sociale de stat.");
-                        ?>
-
-                        <?php
-                           document("https://old.msmps.gov.md/sites/default/files/legislatie/ordin_nr._964_din_020919.pdf", "Ordinul N964 din 02.09.19, cu privire la standardul de supraveghere si dezvoltarii copilului in conditii de ambulator si a Carnetului de dezvoltare a copilului");
-                        ?>
-
-                        <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=110180&lang=ro", "Legea N23 din 16.03.2007, cu privire la profilaxia infectiei HIV/SIDA.");
-                        ?>
-                        <?php
-                           document("https://www.legis.md/cautare/getResults?doc_id=119465&lang=ro", "Legea N411 din 28.03.1995, cu privirea la ocrotirea sanatatii.");
+                            document("https://www.legis.md/cautare/getResults?doc_id=112685&lang=ro", "Codul Familiei al Republicii Moldova");
+                            document("https://www.legis.md/cautare/getResults?doc_id=132934&lang=ro", "Legea nr. 547 din 25.12.2003 Asistența Socială");
+                            document("https://www.legis.md/cautare/getResults?doc_id=146836&lang=ro#", "Legea nr. 140 din 14.06.2011 Privind protecția specială a copiilor aflați în situație de risc și a copiilor separați de părinți");
+                            document("https://www.legis.md/cautare/getResults?doc_id=141516&lang=ro#", "Legea nr. 123 din 18.06.2010 Cu privire la serviciile sociale");
+                            document("https://www.legis.md/cautare/getResults?doc_id=138813&lang=ro#", "Legea nr. 99 din 28.05.2010 Privind regimul juridic al adopției");
+                            document("https://www.legis.md/cautare/getResults?doc_id=140852&lang=ro#", "Legea nr. 338 din 15.12.1994 Privind drepturile copilului");
+                            document("https://www.legis.md/cautare/getResults?doc_id=146155&lang=ro#", "Legea nr. 60 din 30.03.2012 Privind incluziunea socială a persoanelor cu dizabilități");
+                            document("https://www.legis.md/cautare/getResults?doc_id=103705&lang=ro#", "Hotărârea de Guvern nr. 450 din 28.04.2006 Pentru aprobarea Standartelor minime de calitate privind îngrijirea, educarea și socializarea copilului din Centrul de plasament temporar");
+                            document("https://www.legis.md/cautare/getResults?doc_id=101381&lang=ro", "Hotărârea Guvernului nr. 591 din 24 iulie 2017: Aprobă Regulamentul-cadru privind organizarea și funcționarea Serviciilor sociale de tip centru de plasament și standardele minime de calitate.");
+                            document("https://social.gov.md/wp-content/uploads/2024/05/Ordin-nr.-114_24.05.2024_Politica-interna-de-protectie.pdf", "Ordinul nr. 114 din 24 mai 2024: Aprobă Ghidul privind Politica internă de protecție a copilului.");
+                            document("https://www.legis.md/cautare/getResults?doc_id=113032&lang=ro", "Codul Muncii al Republicii Moldova: Aplicabil tuturor angajatorilor, inclusiv instituțiilor sociale de stat.");
+                            document("https://old.msmps.gov.md/sites/default/files/legislatie/ordin_nr._964_din_020919.pdf", "Ordinul N964 din 02.09.19, cu privire la standardul de supraveghere si dezvoltarii copilului in conditii de ambulator si a Carnetului de dezvoltare a copilului");
+                            document("https://www.legis.md/cautare/getResults?doc_id=110180&lang=ro", "Legea N23 din 16.03.2007, cu privire la profilaxia infectiei HIV/SIDA.");
+                            document("https://www.legis.md/cautare/getResults?doc_id=119465&lang=ro", "Legea N411 din 28.03.1995, cu privirea la ocrotirea sanatatii.");
+                            document("https://www.legis.md/cautare/getResults?doc_id=142089&lang=ro", "Legea Nr. 114 din 16.05.2024, cu privire la sănătatea și bunăstarea mintală.");
+                            document("https://odo.md/wp-content/uploads/2024/02/133-legea-protectia-datelor.pdf", "Legea Nr. 133 din 2011, privind protecția datelor cu caracter personal");
+                            document("https://www.legis.md/cautare/getResults?doc_id=109067&lang=ro", "Hotărârea Guvernului Nr. 893/2018 cu privire la aprobarea Programului national de dezinstuționalizare a persoanelor cu dizabilități intelectuale și psihosociale din instituțiile rezidențiale gestionate de Agenția pentru Gestionarea Serviciilor Sociale cu Specializare Înaltă");
+                            document("https://www.legis.md/cautare/getResults?doc_id=119927&lang=ro", "Hotărârea Guvernului Nr. 711/2010 pentru aprobarea Regulamentului-cadru privind organizarea și funcționarea Serviciului social ”Locuință Protejată” și a Standartelor minime de calitate");
+                            document("https://www.legis.md/cautare/getResults?doc_id=119941&lang=ro", "Hotărârea Guvernului Nr. 885 din 28.12.2015 privind aprobarea Regulamentului-cadru privind organizarea și funcționarea Serviciului Social ”Casă Comunitară” și a Standartelor minime de calitate");
                         ?>
 
                     </div>
